74b70f9c8089b52e3f07a3222f5fa71d93fae62e
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-06-19 12:42:10 +0200

    fix analytics

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -11,11 +11,11 @@ export const sendDataToGA = async (creditSum: number) => {
     }-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
 
     await fetch(
-      'https://script.google.com/macros/s/AKfycbxOd_mn9JOFxLEfOr0RJjWB0ywD8_HoTkmBVxVe73GzO8UfngESgqghO_an0ECSqdPI/exec',
+      'https://script.google.com/macros/s/AKfycbw862TqGp_LYbusKISB8t0IkNg_ad_L1YcINX7LPxyD_aQE0GCiDRdB0R_EqHI3rcXQ/exec',
       {
         redirect: 'follow',
         method: 'POST',
-        body: JSON.stringify({ date, credit_sum: creditSum, variant: '2644_1_garant_credit' }),
+        body: JSON.stringify({ date, credit_sum: creditSum, variant: '2644_1_and_garant_credit' }),
         headers: {
           'Content-Type': 'text/plain;charset=utf-8',
         },
